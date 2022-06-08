fdd8bdd4cee200f0cf5307f6e1b7be535e98dc8b
Arturo Cervantes <jesus.cervantes207003@potros.itson.edu.mx>
2022-06-07 23:30:23 -0600

    Proyecto basico React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect, useRef} from "react"
+import { getCharacter, getPeople, searchCharacter } from './api/people';
 
 function App() {
+  const [people, setPeople] = useState([])
+  const [errorState, setErrorState] = useState({hasError: false})
+  const [currentCharacter, setCurrentCharacter] = useState(1)
+  const [details, setDetails] = useState({})
+  const [page, setpage] = useState(1)
+
+
+  const inputSearch = useRef(null)
+  const [textSearch, setTextSearch] = useState("")
+
+
+  useEffect(() => {
+    getPeople(page).then(setPeople)
+    .catch(handleError)
+  }, [page])
+
+  useEffect(() => {
+    getCharacter(currentCharacter).then(setDetails).catch(handleError)
+  }, [currentCharacter])
+
+  const handleError = (err) => {
+    setErrorState({hasError: true, message: err.message})
+  }
+
+  const showDetails = (character) => {
+    const id = Number(character.url.split("/").slice(-2)[0])
+    setCurrentCharacter(id);
+  }
+
+  const onChangeTextSearch = (event) => {
+    event.preventDefault();
+    const text = inputSearch.current.value
+    setTextSearch(text);
+  }
+
+  const onSearchSubmit = (event) => {
+    if (event.key !== "Enter") return;
+
+    inputSearch.current.value = "";
+    setDetails({})
+    searchCharacter(textSearch)
+    .then((setPeople))
+    .catch(handleError)
+  }
+
+  const onChangePage = (next) => {
+    //Search Array methods
+    if(!people.previous && page + next <= 0) return;
+    if(![people.next && page + next >= 9]) return;
+    setpage(page + next);
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <input ref = {inputSearch} 
+    //search for ONCHANGE
+    onChange = {onChangeTextSearch} 
+    onKeyDown = {onSearchSubmit} 
+    type = "text" 
+    placeholder='Search a Character'/>
+    <ul>
+      {errorState.hasError && <div>{errorState.message}</div>}
+      {people?.results?.map((character) =>  (
+       <li key = {character.name} onClick = {showDetails}>{character.name}</li>
+     ))}
+    </ul>
+
+    <section>
+      <button onClick = {() => onChangePage(-1)}>Prev</button>
+      {page}
+      <button onClick = {() => onChangePage(+1)}>Next</button>
+    </section>
+    
+    {details && (
+      //Search Conditionals
+      //Search Aside
+      <aside>
+        <h1>Name: {details.name}</h1>
+        <ul>
+          <li>Weigth: {details.mass}</li>
+          <li>Height: {details.height}</li>
+          <li>Birth: {details.birth_year}</li>
+        </ul>
+      </aside>
+    )}
+    
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import data from './data.json'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Starwars APP', () => {
+  beforeAll(() => jest.spyOn(window, 'fetch'))
+
+  it("Debe de regresar caracteres de la api", async () => {
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => data,
+    })
+
+    render(<App/>)
+
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    expect(window.fetch).toHaveBeenCalledWith("https://swapi.dev/api/people");
+
+    for (let character of data.results){
+      expect(await screen.findByText(character.name)).toBeInTheDocument();
+    }
+  })
+
+  it("Debe de mostrar un error cuando falle la red", async () => {
+    window.fetch.mockRejectedValueOnce(new Error("Network error"));
+    render (<App/>)
+    expect (await screen.findByText("Network error")).toBeInTheDocument();
+  })
+
+  
+
 });
